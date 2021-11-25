c1fd94c6d92ad5f9347d2edafc50df6b7e07f073
Timmsy1998 <jtimms1998@gmail.com>
2021-11-25 03:51:06 +0000

    CRUD for Reply

--- routes/api.php
+++ routes/api.php
@@ -21,4 +21,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResources([
     'question' => QuestionController::class,
     'category' => CategoryController::class,
+    'question/{question}/reply' => ReplyController::class,
 ]);
