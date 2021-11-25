1a835bf8615499039b648e76dffea4aae5cb8201
Timmsy1998 <jtimms1998@gmail.com>
2021-11-25 04:35:36 +0000

    Implemented JWT into the Project for Secure Authentication

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,17 @@ Route::apiResources([
 
 Route::post('/like/{reply}', 'LikeController@LikeIt');
 Route::delete('/like/{reply}', 'LikeController@UnLikeIt');
+
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
